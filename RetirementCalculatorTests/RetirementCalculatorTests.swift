388f63b44bf12553673df3e32ee2c2ffe2a84ea1
Nadeesha Lakmal <nadeeshac32@gmail.com>
2024-02-14 13:13:32 +0800

    adding back unit tests

--- RetirementCalculatorTests/RetirementCalculatorTests.swift
+++ RetirementCalculatorTests/RetirementCalculatorTests.swift
@@ -20,15 +20,15 @@ final class RetirementCalculatorTests: XCTestCase {
     }
 
     func testExample() throws {
-//        let retirementAmount = sut?.calculateRetirementAmount(current_age: 32, retirement_age: 42, monthly_investment: 1000, current_savings: 1000, interest_rate: 10)
-//        XCTAssertEqual(retirementAmount, 92915861.853888571)
+        let retirementAmount = sut?.calculateRetirementAmount(current_age: 32, retirement_age: 42, monthly_investment: 1000, current_savings: 1000, interest_rate: 10)
+        XCTAssertEqual(retirementAmount, 92915861.853888571)
     }
 
     func testPerformanceExample() throws {
-//        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
     }
 
 }
